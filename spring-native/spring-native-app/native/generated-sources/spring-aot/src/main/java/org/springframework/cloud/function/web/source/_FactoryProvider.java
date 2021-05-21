cb7f4221a9921e85b8ffad0639b6b179777fce67
Dan Dobrin <dan.dobrin@gmail.com>
2021-05-21 07:40:37 -0400

    qconplus materials

---
+++ spring-native/spring-native-app/native/generated-sources/spring-aot/src/main/java/org/springframework/cloud/function/web/source/_FactoryProvider.java
@@ -0,0 +1,7 @@
+package org.springframework.cloud.function.web.source;
+
+public abstract class _FactoryProvider {
+  public static FunctionExporterInitializer functionExporterInitializer() {
+    return new FunctionExporterInitializer();
+  }
+}
